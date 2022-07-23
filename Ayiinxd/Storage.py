9504d597514822fa5163df16f450bdb70a58bb02
daps <syadisini3@gmail.com>
2022-07-23 08:52:57 +0000

    daps-Userbot: auto fixes
    Signed-off-by: daps <syadisini3@gmail.com>

--- Ayiinxd/Storage.py
+++ Ayiinxd/Storage.py
@@ -1,8 +1,8 @@
-dd# This Source Code Form is subject to the terms of the Mozilla Public
+from pathlib import Path
+import json
+dd  # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import json
-from pathlib import Path
 
 FILE_NAME = "data.json"
 
