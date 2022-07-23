beabd3cb7287e5487c10cd8ca9d19b344909c76f
dapsya <108969276+dapsya@users.noreply.github.com>
2022-07-23 22:54:50 +0700

    Update paste.py

--- Daps/dap/paste.py
+++ Daps/dap/paste.py
@@ -2,8 +2,8 @@ import json
 
 import requests
 
-from AyiinXd import BOTLOG_CHATID
-from AyiinXd.ayiin.logger import logging
+from Daps import BOTLOG_CHATID
+from Daps.ayiin.logger import logging
 
 LOGS = logging.getLogger("AyiinUserBot")
 
@@ -31,7 +31,7 @@ async def p_paste(message, extension=None):
             else f"https://pasty.lus.pm/{response['id']}.txt"
         )
         try:
-            from AyiinXd import bot
+            from Daps import bot
 
             await bot.send_message(
                 BOTLOG_CHATID,
